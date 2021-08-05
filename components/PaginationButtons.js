5f18ee2ae47cafd20a4c345b0f0928c4913f4438
Caolan Fanning <caolan.fanning@gmail.com>
2021-08-05 12:11:13 +0100

    save

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -1,5 +1,32 @@
+import { useRouter } from "next/router";
+import Link from "next/router";
+
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
+
 function PaginationButtons() {
-	return <div>pagination</div>;
+	const router = useRouter();
+
+	const startIndex = Number(router.query.start) || 0;
+
+	return (
+		<div>
+			{startIndex >= 10 && (
+				<Link
+					href={`/search?term=${router.query.term}&start=${startIndex - 10}`}>
+					<div>
+						<ChevronLeftIcon className='h-4' />
+						<p>Previous</p>
+					</div>
+				</Link>
+			)}
+			<Link href={`/search?term=${router.query.term}&start=${startIndex + 10}`}>
+				<div>
+					<ChevronRightIcon className='h-4' />
+					<p>Next</p>
+				</div>
+			</Link>
+		</div>
+	);
 }
 
 export default PaginationButtons;
